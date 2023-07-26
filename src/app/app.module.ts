de410d7d38c26470fd1030436b619d4f6e58db71
Paolo Missagia <login@paolomissagia.com>
2023-07-26 22:44:14 +0100

    updated app root component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { MenuOptionComponent } from './components/shared/menu-option/menu-option.component';
+import { LevelsComponent } from './components/levels/levels.component';
+import { GuideComponent } from './components/guide/guide.component';
+import { GuideItemComponent } from './components/shared/guide-item/guide-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    CategoriesComponent,
+    MenuOptionComponent,
+    LevelsComponent,
+    GuideComponent,
+    GuideItemComponent
   ],
   imports: [
     BrowserModule,
